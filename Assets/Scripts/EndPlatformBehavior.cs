35d8ea2d696af3b7c863c9c652501ef8c4435f37
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-08-22 15:25:11 +0100

    Pre HDRP

---
+++ Assets/Scripts/EndPlatformBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPlatformBehavior : MonoBehaviour
+{
+    [SerializeField] GameObject light;
+    private void Update() {
+        light.GetComponent<Light>().intensity = 2 + Mathf.Sin(Time.time*2);
+    }
+}
