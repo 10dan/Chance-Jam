acc92c06a38ec909a44f33eea404eab99dff753b
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-08-28 12:39:58 +0100

    Platform works
    end platform knows what nmber ther dice was when it rolled onit.

--- Assets/Scripts/DiceRoller.cs
+++ Assets/Scripts/DiceRoller.cs
@@ -5,26 +5,24 @@ using UnityEngine;
 
 public class DiceRoller : MonoBehaviour {
     Transform[] children;
+    public int currentTop = 0;
     private void Start() {
         children = new Transform[transform.childCount];
-        for(int i = 0; i < gameObject.transform.childCount; i ++) {
+        for (int i = 0; i < gameObject.transform.childCount; i++) {
             children[i] = gameObject.transform.GetChild(i);
         }
     }
-    private void Update() {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb.velocity.magnitude < Mathf.Epsilon) { //Once it stops moving.
-            float maxY = -Mathf.Infinity;
-            int topFace = 0;
-            int count = 0;
-            foreach(Transform t in children) {
-                if(t.position.y > maxY) {
-                    maxY = t.position.y;
-                    topFace = count;
-                }
-                count++;
+    private void Update() { 
+        float maxY = -Mathf.Infinity;
+        int topFace = 0;
+        int count = 0;
+        foreach (Transform t in children) {
+            if (t.position.y > maxY) {
+                maxY = t.position.y;
+                topFace = count;
             }
-            print(topFace + 1);
+            count++;
         }
+        currentTop = topFace + 1;
     }
 }
--- Assets/Scripts/EndPlatformBehavior.cs
+++ Assets/Scripts/EndPlatformBehavior.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EndPlatformBehavior : MonoBehaviour
 {
     [SerializeField] GameObject light;
+    [SerializeField] DiceRoller dice;
     [SerializeField] float initIntensity;
     [SerializeField] float changeIntensity;
     [SerializeField] float speed;
     private void Update() {
         light.GetComponent<Light>().intensity = initIntensity + changeIntensity * Mathf.Sin(Time.time*speed);
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.collider.gameObject.name == "Dice") {
+            print(dice.currentTop);
+        }
     }
 }
